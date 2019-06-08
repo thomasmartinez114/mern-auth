9fd807c4a8b6e11da107845da71fcc8b2b5ffa27
unknown <tmartinez114@gmail.com>
2019-06-08 12:38:52 -0400

    updated

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoDBUrl: 'mongodb+srv://tmartinez114:Password123@cluster0-82da4.mongodb.net/myexpressapp?retryWrites=true',
-    secretOrKey: 'secret'
+    mongoDBUrl: 'mongodb+srv://tmartinez114:Zeneida1965@auth-hbunw.mongodb.net/mern-auth?retryWrites=true&w=majority
+    '
 
 };
