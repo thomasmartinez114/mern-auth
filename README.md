1a5956f30e648f82ea23e9a6942fc2d107511641
unknown <tmartinez114@gmail.com>
2019-06-17 09:12:03 -0400

    updated README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Followed tutorial by: https://blog.bitsrc.io/build-a-login-auth-app-with-mern-stack-part-1-c405048e3669
+Tutorial by: https://blog.bitsrc.io/build-a-login-auth-app-with-mern-stack-part-1-c405048e3669
 
 ![](/logo/main.png)
 ![](/logo/second.png)
--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const passport = require('passport');
-
-const users = require('./routes/api/users');
-
 const app = express();
-
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -14,28 +9,12 @@ app.use(
   })
 );
 app.use(bodyParser.json());
-
 // DB Config
-const db = require("./config/keys").mongoDBUrl;
-
+const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
-  
-// Passport middleware
-app.use(passport.initialize());
-
-// Passport config
-require('./config/passport') (passport);
-
-// Routes
-app.use('/api/users', users);
-  
-const port = process.env.PORT || 8080; // process.env.port is Heroku's port if you choose to deploy the app there
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
