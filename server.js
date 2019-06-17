ab5348d734f463a2e1763ef895af6629c1a50b76
unknown <tmartinez114@gmail.com>
2019-06-17 16:05:11 -0400

    added User schema in /models || login validators in /validation

--- server.js
+++ server.js
@@ -17,4 +17,4 @@ mongoose
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server up and running on port ${port}!`));
+app.listen(port, () => console.log(`Server is running on port ${port}!`));
