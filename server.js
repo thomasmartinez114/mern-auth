8a2caa5f87089505fe2f21d30103225a01bab6c5
unknown <tmartinez114@gmail.com>
2019-06-20 11:48:36 -0400

    added const for passport & users || passport middleware

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
 const app = express();
 
-// Bodyparser middleware
+// Bo ndyparser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -14,6 +18,15 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
